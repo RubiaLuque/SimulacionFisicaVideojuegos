b9d433bde64ad60f5048347dcd9000b829c57064
RubiaLuque <prubia01@ucm.es>
2023-09-29 09:39:17 +0200

    particula con aceleracion

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -3,6 +3,7 @@
 
 Particle::Particle(Vector3 pos, Vector3 vel, Vector3 acc, double radius) {
 	this->pos = pos;
+	this->acc = acc;
 	transform = PxTransform(pos.x, pos.y, pos.z);
 	physx::PxShape* sphere = CreateShape(PxSphereGeometry(radius));
 	renderItem = new RenderItem(sphere, &transform, Vector4(1.0, 1.0, 1.0, 1.0));
@@ -15,7 +16,8 @@ Particle::~Particle() {
 }
 
 void Particle::update(double t) {
-	pos = pos + vel * t;
+	vel += acc * t;
+	vel *= powf(DUMPING, t);
+	pos = pos + vel * t; 
 	transform.p.x = pos.x; transform.p.y = pos.y; transform.p.z = pos.z;
-	//renderItem->transform = &transform;
 }
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -8,9 +8,11 @@ class Particle
 {
 private:	
 	Vector3 vel;
+	Vector3 acc;
 	Vector3 pos;
 	PxTransform transform;
 	RenderItem* renderItem;
+	const double DUMPING = 0.998; //limita la velocidad para que no se dispare por errores numericos
 
 public:
 	Particle(Vector3 pos, Vector3 vel, Vector3 acc, double radius);
